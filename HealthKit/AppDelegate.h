f483c6feca6c9ddb3891e1cf5db0eb55d7976375
Stephen Printup <stephen.printup@gmail.com>
2016-01-22 13:58:24 -0700

    Initial Commit

---
+++ HealthKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HealthKit
+//
+//  Created by Stephen Printup on 1/22/16.
+//  Copyright © 2016 Stephen Printup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HealthKit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HealthKit
+//
+//  Created by Stephen Printup on 1/22/16.
+//  Copyright © 2016 Stephen Printup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
